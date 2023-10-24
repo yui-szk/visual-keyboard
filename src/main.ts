472c9edd3bb08dab1429197a8fc8f3abe6e8ed83
yui-szk <yuisuzuki.017@gmail.com>
2023-10-24 15:46:27 +0900

    完成 v0

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,45 @@
 // 元のソースコード
 // https://github.com/NasuPanda/frontend-practices/blob/main/10-vanilla-projects/Virtual-Keyboard/Keyboard/Keyboard.js
 
-type KeyboardType = {
+type Keyboard = {
   elements: {
     main: HTMLDivElement | null;
     keysContainer: HTMLDivElement | null;
-    keys: string[];
+    keys: NodeListOf<Element> | null;
   };
   eventHandlers: {
-    oninput: null;
-    onclose: null;
+    oninput: ((value: string) => void) | null;
+    onclose: ((value: string) => void) | null;
   };
   properties: {
     value: string;
     capsLock: boolean;
   };
   init: () => void; //返り値なし
-  _createKeys: () => void;
-  _triggerEvent: (handlerName: string) => void;
+  _createKeys: () => DocumentFragment;
+  _triggerEvent: (handlerName: "oninput" | "onclose") => void;
   _toggleCapsLock: () => void;
-  open: (initialValue: string, oninput: string, onclose: string) => void;
+  open: (
+    initialValue: string,
+    oninput: (value: string) => void,
+    onclose?: (value: string) => void
+  ) => void;
   close: () => void;
 };
 
-const Keyboard: KeyboardType = {
+// interface HTMLButtonElementEvent<T extends HTMLButtonElement> extends MouseEvent {
+//   target: T;
+// }
+type HTMLButtonElementEvent = MouseEvent & {
+  target: HTMLButtonElement;
+};
+
+const keyboard: Keyboard = {
   elements: {
     // null = 空っぽ
     main: null,
     keysContainer: null,
-    keys: [],
+    keys: null,
   },
 
   eventHandlers: {
@@ -44,16 +55,37 @@ const Keyboard: KeyboardType = {
 
   init() {
     // main要素を制作
-    this.elements.main = document.createElement("div");
+    this.elements.main = document.querySelector(".keyboard");
     this.elements.keysContainer = document.createElement("div");
 
     // main要素をセットアップ
-    this.elements.main.classList.add("Keyboard", "1keyboard--hidden");
+    // this.elements.main.classList.add("Keyboard", "keyboard--hidden");
     this.elements.keysContainer.classList.add("keyboard__keys");
+    this.elements.keysContainer.appendChild(this._createKeys());
+
+    // keysにkeyElementを追加
+    this.elements.keys =
+      this.elements.keysContainer.querySelectorAll(".keyboard__key");
 
     // DOMの追加
-    this.elements.main.appendChild(this.elements.keysContainer);
-    document.body.appendChild(this.elements.main);
+    // this.elements.main?.appendChild(this.elements.keysContainer);
+    this.elements.main?.appendChild(this.elements.keysContainer);
+    // document.body.appendChild(this.elements.main);
+
+    // 特定のクラスを持つ要素では自動的にキーボードを使う
+    const textareaElement = document.querySelector<HTMLTextAreaElement>(
+      ".use-keyboard-input"
+    );
+    textareaElement?.addEventListener("focus", () => {
+      this.elements.main?.classList.add("keyboard--show");
+      this.open(textareaElement.value, (currentValue) => {
+        textareaElement.value = currentValue;
+      });
+    });
+
+    // document.getElementsByClassName("text");
+    // // querySelector=指定したclass/IDの最初の文字列を指定
+    // document.querySelector<HTMLParagraphElement>("text")?.innerHTML = "a"
   },
   _createKeys() {
     const fragment = document.createDocumentFragment();
@@ -67,7 +99,8 @@ const Keyboard: KeyboardType = {
     ];
 
     const createIconHTML = (icon_name: string) => {
-      return `<i class="material-icons>${icon_name}</i>`;
+      console.log("createIconHTML", icon_name);
+      return `<i class="material-icons">${icon_name}</i>`;
     };
 
     keyLayout.forEach((key) => {
@@ -75,29 +108,138 @@ const Keyboard: KeyboardType = {
       const insertLineBreak =
         ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
 
-      // 属性/クラスを追加
+      // type="button" を追加
       keyElement.setAttribute("type", "button");
-      //全てのキーに必要なクラス
+      // class="keyboard__key" を追加
       keyElement.classList.add("keyboard__key");
+      keyElement.setAttribute("value", key);
 
       switch (key) {
         case "backspace":
           keyElement.classList.add("keyboard__key--wide");
           keyElement.innerHTML = createIconHTML("backspace");
+
+          keyElement.addEventListener("click", () => {
+            // 0からvalue-1の文字までをvalueに代入
+            this.properties.value = this.properties.value.substring(
+              0,
+              this.properties.value.length - 1
+            );
+            this._triggerEvent("oninput");
+          });
+          break;
+
+        case "caps":
+          keyElement.classList.add(
+            "keyboard__key--wide",
+            "keyboard__key--can-activate"
+          );
+          keyElement.innerHTML = createIconHTML("keyboard_capslock");
+
+          keyElement.addEventListener("click", () => {
+            this._toggleCapsLock();
+            keyElement.classList.toggle(
+              "keyboard__key--active",
+              this.properties.capsLock
+            );
+          });
+          break;
+
+        case "enter":
+          keyElement.classList.add("keyboard__key--wide");
+          keyElement.innerHTML = createIconHTML("keyboard_return");
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value += "\n";
+            this._triggerEvent("oninput");
+          });
+          break;
+
+        case "space":
+          keyElement.classList.add("keyboard__key--extra--wide");
+          keyElement.innerHTML = createIconHTML("space_bar");
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value += " ";
+            this._triggerEvent("oninput");
+          });
+          break;
+
+        case "done":
+          keyElement.classList.add(
+            "keyboard__key--wide",
+            "keyboard__key--dark"
+          );
+          keyElement.innerHTML = createIconHTML("check_circle");
+
+          keyElement.addEventListener("click", () => {
+            this.close();
+            this._triggerEvent("onclose");
+          });
+          break;
+
+        default:
+          keyElement.textContent = key.toLowerCase();
+
+          keyElement.addEventListener("click", (event: MouseEvent) => {
+            if (event.target instanceof HTMLButtonElement) {
+              this.properties.value += this.properties.capsLock
+                ? key.toUpperCase()
+                : key.toLowerCase();
+              this._triggerEvent("oninput");
+              console.log(this.properties.value);
+            }
+          });
+          break;
+      }
+
+      fragment.appendChild(keyElement);
+
+      if (insertLineBreak) {
+        fragment.appendChild(document.createElement("br"));
       }
     });
+
+    return fragment;
   },
+
   _triggerEvent(handlerName) {
+    if (typeof this.eventHandlers[handlerName] == "function") {
+      this.eventHandlers[handlerName]?.(this.properties.value);
+    }
     console.log("Event Triggered! Event Name:" + handlerName);
   },
   _toggleCapsLock() {
+    this.properties.capsLock = !this.properties.capsLock;
+
+    // if->()が0以外の場合、1を返して実行、0の場合実行されない
+    // nullは0値
+    if (this.elements.keys) {
+      for (const key of this.elements.keys) {
+        if (key.childElementCount === 0 && key.textContent) {
+          key.textContent = this.properties.capsLock
+            ? key.textContent.toUpperCase()
+            : key.textContent.toLowerCase();
+        }
+      }
+    }
     console.log("Caps Lock Toggled!");
   },
   // textarea に既に入力があった場合、その入力をスタート値にする
-  open(initialValue, oninput, onclose) {},
-  close() {},
+  open(initialValue, oninput, onclose) {
+    this.properties.value = initialValue || "";
+    this.eventHandlers.oninput = oninput;
+    this.eventHandlers.onclose = onclose ?? null; // onclose ? onclose: null
+    this.elements.main?.classList.remove("keyboard--hidden");
+  },
+  close() {
+    this.properties.value = "";
+    this.eventHandlers.oninput = null;
+    this.eventHandlers.onclose = null;
+    this.elements.main?.classList.remove("keyboard--show");
+  },
 };
 
-window.addEventListener("DOMContentLoaded", function () {
-  Keyboard.init();
+window.addEventListener("DOMContentLoaded", () => {
+  keyboard.init();
 });
